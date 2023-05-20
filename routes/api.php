91d91467f55e9c4b62d0aff3736365170417661f
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-05-20 13:12:30 -0300

    [Route API: Step 1]: create register and login as public routes. And logout as protected routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+// Root route
+Route::get('/', function() {
+    return response()->json([
+        'message' => 'Bem vindo a api do CSS Dictionary'
+    ]);
+});
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
