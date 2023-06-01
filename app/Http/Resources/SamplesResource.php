713c50fa32f8c801250315d572a5baa9f5e42df7
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-06-01 19:32:19 -0300

    [DateFormat Step:2] Use DateFormatWithMessage inside the SamplesResource

--- app/Http/Resources/SamplesResource.php
+++ app/Http/Resources/SamplesResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Utils\DateFormatWithMessage;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,11 +15,14 @@ class SamplesResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $objDateFormat = new DateFormatWithMessage($this->created_at);
+        
         return [
             'id' => (string) $this->id,
             'attributes' => [
                 'title' => $this->title,
                 'description' => $this->description,
+                'created_at' => $objDateFormat->message
             ],
             'display' => [
                 'title' => 'Example',
