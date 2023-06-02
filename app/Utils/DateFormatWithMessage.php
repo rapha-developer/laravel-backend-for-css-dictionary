d22cb589959b8a6dda47b868576bc53f92093c66
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-06-02 11:16:20 -0300

    [DateFormat Step:1] Refactor DateFormatWithMessage: add two new functions, change variables names or function names, also logic inside the functions, make comments on each function explaining it and showing what it returns.

--- app/Utils/DateFormatWithMessage.php
+++ app/Utils/DateFormatWithMessage.php
@@ -14,24 +14,55 @@ class DateFormatWithMessage {
     protected int $pastYears;
 
     public string $message = "";
-    public const ANY_DIFFERENCE = 0;
+    public const NO_DIFFERENCE = 0;
+    public const ONE = 1;
 
     public function __construct($created_at)
     {
         $this->now = Carbon::parse(Carbon::now());
         $this->dateStored = Carbon::parse($created_at);
         
-        if ($this->dateStored->isToday()) {
-            $this->message = 'Today';
-            return ;
-        }
+        $this->checkIfIsToday();
+        $this->checkIfIsYesterday();
         $this->run();
     }
+
+    /**
+     *  Check if date of user is today
+     *  @return void
+     */
+    protected function checkIfIsToday() {
+        if ($this->dateStored->isToday()) {
+            $this->message = 'today';
+            return ;
+        }
+    }
+    
+    /**
+     *  Check if date of user is yesterday
+     *  @return void
+     */
+    protected function checkIfIsYesterday() {
+        if ($this->dateStored->isYesterday()) {
+            $this->message = 'yesterday';
+            return ;
+        }
+    }
+
+    /**
+     *  Run main function to determine how many days, weeks, months, years ago is this date
+     *  @return void
+     */
     protected function run() {
         $this->fillPastTimesVariables();
         $this->generateAllPastTimesMessages();
-        $this->removeAllTimesUnnecessary();
+        $this->setMessageWithCustomValidDateMessage();
     }
+
+    /**
+     *  Fill {pastDays, pastWeeks, pastMonths, pastYears}
+     *  @return void
+     */
     protected function fillPastTimesVariables() 
     {
         $this->pastDays   = $this->now->diffInDays($this->dateStored);
@@ -39,31 +70,41 @@ class DateFormatWithMessage {
         $this->pastMonths = $this->now->diffInMonths($this->dateStored);
         $this->pastYears  = $this->now->diffInYears($this->dateStored);
     }
+
+    /**
+     *  Generate all custom messages for different time frames. 
+     *  @return Array
+     */
     protected function generateAllPastTimesMessages() {
         return [
             'past_days' => [
                 'amount' => $this->pastDays,
-                'message' => $this->pastDays . ' days ago'
+                'message' => ($this->pastDays === static::ONE) ? ' day ago' : ' days ago'
             ],
             'past_weeks' => [
                 'amount' => $this->pastWeeks,
-                'message' => $this->pastWeeks . ' weeks ago'
+                'message' => ($this->pastWeeks === static::ONE) ? ' week ago' : ' weeks ago'
             ],
             'past_months' => [
                 'amount' => $this->pastMonths,
-                'message' => $this->pastMonths . ' months ago'
+                'message' => ($this->pastMonths === static::ONE) ? ' month ago' : ' months ago'
             ],
             'past_years' => [
                 'amount' => $this->pastYears,
-                'message' => $this->pastYears . ' years ago'
+                'message' => ($this->pastYears === static::ONE) ? ' year ago' : ' years ago'
             ]
         ];
     }
-    protected function removeAllTimesUnnecessary() {
+
+    /**
+     *  Set Message with custom valid date Message.
+     *  @return void
+     */
+    protected function setMessageWithCustomValidDateMessage() {
         $allPastTimes = $this->generateAllPastTimesMessages();
-        foreach ($allPastTimes as $pastName => $pastTime) {;
-            if($pastTime['amount'] !== static::ANY_DIFFERENCE) {
-                $this->message = $pastTime['message'];
+        foreach ($allPastTimes as $pastName => $pastTime) {
+            if($pastTime['amount'] !== static::NO_DIFFERENCE) {
+                $this->message = $pastTime['amount'] . $pastTime['message'];
             }
         }
     }
